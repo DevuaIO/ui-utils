5877523ee546765b9632b1c65c490073e8d57a31
DevuaIO <dev.devua@outlook.com>
2026-05-30 21:25:05 +0300

    fix(react): added `emit` function to exports from library

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,4 @@
 export { useDebounce } from "./hooks/useDebounce";
-export { useEvent } from "./hooks/useEvent";
+export { emit, useEvent } from "./hooks/useEvent";
 export { PING_ID, type PingKey, ping, pingKey, usePing } from "./hooks/usePing";
 export { useThrottle } from "./hooks/useThrottle";
